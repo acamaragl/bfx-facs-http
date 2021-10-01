aed983aa7a52fa0e1086917213a68dd02cbe4772
vigan-abd <vigan.abd@gmail.com>
2021-10-01 15:41:37 +0200

    added another test for qs

--- test/http.facility.test.js
+++ test/http.facility.test.js
@@ -363,6 +363,9 @@ describe('http facility tests', () => {
       resp = await fac.request('/foo/bar/9', { method: 'get', encoding: 'json', qs: 'a=1&b=c' })
       expect(resp.body).to.be.deep.equal({ a: '1', b: 'c' })
 
+      resp = await fac.request('/foo/bar/9?', { method: 'get', encoding: 'json', qs: 'a=1&b=c' })
+      expect(resp.body).to.be.deep.equal({ a: '1', b: 'c' })
+
       resp = await fac.request('/foo/bar/9', { method: 'get', encoding: 'json', qs: 123 })
       expect(resp.body).to.be.deep.equal({ 123: '' })
     })
